853eb9e6b6458ccb131c31a0df33eb76d52b9a52
John Hageman <jhageman@indeed.com>
2018-11-21 10:23:07 -0600

    Adding more to HTML DSL template

--- src/main/kotlin/com/hagemajr/todo/templates/TodoTemplates.kt
+++ src/main/kotlin/com/hagemajr/todo/templates/TodoTemplates.kt
@@ -3,6 +3,7 @@ package com.hagemajr.todo.templates
 import io.ktor.html.*
 import kotlinx.html.HTML
 import kotlinx.html.*
+import org.intellij.lang.annotations.Flow
 
 
 class MulticolumnTemplate(val main: MainTemplate = MainTemplate()) : Template<HTML> {
@@ -10,6 +11,10 @@ class MulticolumnTemplate(val main: MainTemplate = MainTemplate()) : Template<HT
     val column2 = Placeholder<FlowContent>()
     override fun HTML.apply() {
         insert(main) {
+            header {
+                headerTitle { +"ToDo List"}
+                headerSubtitle { +"My Todo Implementation"}
+            }
             menu {
                 item { +"One" }
                 item { +"Two" }
@@ -22,40 +27,106 @@ class MulticolumnTemplate(val main: MainTemplate = MainTemplate()) : Template<HT
                     insert(column2)
                 }
             }
+            footer {
+                footerContent { +"A POC by John Hageman" }
+            }
         }
     }
 }
 
 class MainTemplate : Template<HTML> {
+    val header = TemplatePlaceholder<HeaderTemplate>()
     val content = Placeholder<HtmlBlockTag>()
+    val appBody = TemplatePlaceholder<BodyTemplate>()
     val menu = TemplatePlaceholder<MenuTemplate>()
+    val footer = TemplatePlaceholder<FooterTemplate>()
     override fun HTML.apply() {
         head {
             title { +"Template" }
-            link(rel = "stylesheet", href="/static/bulma.min.css")
+            link(rel = "stylesheet", href = "/static/bulma.min.css")
         }
         body {
-            div(classes = "container"){
-                section(classes = "hero is-info is-bold is-rounded"){
-                    div(classes = "hero-body"){
-                        div("container"){
-                            h1("title"){
-                                +"Todo List"
-                            }
-                            h2("subtitle"){
-                                +"My Todo Implementation"
-                            }
-                        }
-                    }
+            div(classes = "container") {
+                insert(HeaderTemplate(), header)
+                insert(BodyTemplate(), appBody)
+                h1 {
+                    insert(content)
                 }
+                insert(MenuTemplate(), menu)
+                insert(FooterTemplate(), footer)
             }
-            h1 {
-                insert(content)
-            }
-            insert(MenuTemplate(), menu)
         }
     }
 }
+
+class HeaderTemplate : Template<FlowContent> {
+    val headerTitle = Placeholder<HtmlBlockTag>()
+    val headerSubtitle = Placeholder<HtmlBlockTag>()
+    override fun FlowContent.apply() {
+
+        section(classes = "hero is-info is-bold is-rounded") {
+            div(classes = "hero-body") {
+                div("container") {
+                    h1("title") {
+                        insert(headerTitle)
+                    }
+                    h2("subtitle") {
+                        insert(headerSubtitle)
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+class BodyTemplate : Template<FlowContent>{
+    val inputTemplate = TemplatePlaceholder<InputTemplate>()
+    override fun FlowContent.apply(){
+        div(classes = "columns is-centered"){
+            div(classes = "column is-half"){
+                div(classes = "section"){
+                    insert(InputTemplate(), inputTemplate)
+                }
+            }
+        }
+    }
+}
+
+class InputTemplate : Template<FlowContent>{
+    override fun FlowContent.apply(){
+        div(classes = "field has-addons"){
+            p(classes = "control is-expanded"){
+                input(classes = "input", type = InputType.text){
+                    placeholder = "What do you need to do?"
+                }
+            }
+            p(classes = "control"){
+                a(classes = "button is-info"){
+                    +"Add Todo"
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+class FooterTemplate : Template<FlowContent>{
+    val footerContent = Placeholder<HtmlBlockTag>()
+    override fun FlowContent.apply(){
+        footer(classes = "footer") {
+            div(classes = "content has-text-centered"){
+                p {
+                    insert(footerContent)
+                }
+            }
+        }
+    }
+}
+
 
 class MenuTemplate : Template<FlowContent> {
     val item = PlaceholderList<UL, FlowContent>()
